f7c3be5c16452ac0fc240b188e36d901ff6a29c2
Peter Yoachim <yoachim@uw.edu>
2015-12-28 15:38:33 -0800

    US inst selection now works

--- python/checkUSIdent.py
+++ python/checkUSIdent.py
@@ -6,6 +6,18 @@ from grabPhdClass import checkUSAff
 
 # check that the names in simpleUSList.dat correctly identify US institutions
 
-allusInst = np.genfromtxt('usInst.dat', dtype='|S50', delimiter='$$', comments='#')
+allusInst = np.genfromtxt('usInst.dat', dtype='|S100', delimiter='$$', comments='#')
 
-results = [checkUSAff(aff) for aff in allusInst]
+results = np.array([checkUSAff(aff) for aff in allusInst])
+
+print 'US institions which did not pass the test that should have'
+print allusInst[np.where(results == False)]
+print '--------'
+
+allInst = np.genfromtxt('allInst.dat', dtype='|S100', delimiter='$$', comments='#')
+
+forignInst = np.array([inst for inst in allInst if inst not in allusInst])
+results = np.array([checkUSAff(aff) for aff in forignInst])
+print 'non-US institutions that passed the test that should not have'
+print forignInst[np.where(results == True)]
+print '--------'
