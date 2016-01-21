ee3fbe85bd6441126f4a22a5d8baaa1f40b1468f
Peter Yoachim <yoachim@uw.edu>
2016-01-20 22:24:21 -0800

    start making pdf plots

---
+++ python/combine.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Combine all the data files together
+
+echo 'phd aff; latest year; latest aff; latest 1st year; latest 1st aff; largest publication gap; numRecords; numLinked; uniqueName; latest year unlinked; noAstroJournal; nonUS' > output/all_years.dat
+
+ls output/1*.dat | xargs -I'{}' tail -n +2 '{}' >>  output/all_years.dat
+ls output/2*.dat | xargs -I'{}' tail -n +2 '{}' >>  output/all_years.dat
--- python/makePlots.py
+++ python/makePlots.py
@@ -3,42 +3,69 @@ from runYear import readYear
 import glob
 import matplotlib.pylab as plt
 
-files = glob.glob('output/*.dat')
-years = [int(filename[-8:-4]) for filename in files]
 
+# Read in all the data
+data = readYear(0, filename = 'output/all_years.dat')
+
+# Make a histogram of number of PhDs per year
+good = np.where((data['noAstroJournal'] == 'None') &
+                (data['nonUS'] == 'None'))
+bins = np.arange(data['phd_year'][good].min()-.5, data['phd_year'][good].max()+1.5,1)
+fig,ax = plt.subplots()
+blah = ax.hist(data['phd_year'][good], bins)
+ax.set_xlabel('Year')
+ax.set_ylabel('Number of US Astro PhDs')
+fig.savefig('../plots/phdsperyear.pdf')
+plt.close(fig)
+
+
+# Make a handy array
+bins = np.arange(0,30,1)-0.5
+years = np.unique(data['phd_year'])
+numberActive = np.zeros((years.size,bins.size),float)
+numberPossible = np.zeros((years.size,bins.size),float)
+
+lastYearDist = data['latest_year'][good]-data['phd_year'][good]
+for i,year in enumerate(years):
+    gg = np.where(data['phd_year'][good] == year)
+    droppedHist,bins = np.histogram(lastYearDist[gg],bins)
+    droppedHist = np.add.accumulate(droppedHist)
+    classSize = gg[0].size
+    numberActive[i][0] = classSize
+    maxIndex = 2015-year
+    numberActive[i][1:maxIndex+1] = classSize-droppedHist[0:maxIndex]
+    numberPossible[i][0:maxIndex+1] = classSize
+
+
+
+# Let's bin things up by 2's
+binsize = 2
+year_mins = np.arange(1999,2011+binsize,binsize)
+year_maxes = np.arange(2000,2012+binsize,binsize)
+fig,ax = plt.subplots()
 bins = np.arange(0,30,1)-0.5
 
-years = [1999,2000,2001, 2009,2010, 2011, 2012]
-#years = [1995,1996,1997,1998]
-first = True
-for year in years:
-    data = readYear(year)
-    good = np.where( (data['noAstroJournal'] != 'True') & (data['nonUS'] != 'True'))
 
-    print '%i: %i' % (year, np.size(good[0]))
-    nDiss = good[0].size
+# OK, so I need for each integer year, the number of possible people still active, and the actual number still active
 
-    yearDist = np.sort(data['latest_year'][good]-year)+1
-    droppedHist,bins = np.histogram(yearDist,bins)
-    stillActive = nDiss-np.add.accumulate(droppedHist)
+colors = [ plt.cm.jet(x) for x in np.linspace(0, 1, year_mins.size) ]
 
-    good = np.where(stillActive > 0)
+for ymin,ymax,color in zip(year_mins,year_maxes,colors):
+    gg = np.where((years >= ymin) & (years <= ymax))
+    na = numberActive[gg,:].sum(axis=1).ravel()
+    npos = numberPossible[gg,:].sum(axis=1).ravel()
+    good = np.where(npos > 0)
+    curve = na[good]/npos[good]
 
-    if first:
-        label = 'PhD year = %i' % year
-        first = False
-    else:
-        label = year
+    ax.plot(bins[:-1][good]+.5, curve,
+            label='%i-%i' % (ymin,ymax), color=color)
 
-    # add the [:-1] to crop off 2016.
-    plt.plot(bins[:-1][good][:-1]+.5, stillActive[good][:-1]/float(nDiss), label=label)
+ax.set_xlabel('Years post PhD')
+ax.set_ylabel('Fraction Still Active in ADS')
+ax.legend()
+ax.set_title('Astronomy PhD Retention')
 
-plt.xlabel('Years post PhD')
-plt.ylabel('Fraction Still Active in ADS')
-plt.legend()
-plt.title('Astronomy PhD Retention')
-
-
+fig.savefig('../plots/active_curves.pdf')
 
 # Next up.
 # 1) Bin into multiple years.
