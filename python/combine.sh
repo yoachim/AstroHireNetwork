e0433beaea61c2610617a26c7603db06f7a94587
Peter Yoachim <yoachim@uw.edu>
2016-01-27 21:39:33 -0800

    rescrape complete

--- python/combine.sh
+++ python/combine.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # Combine all the data files together
 
-echo 'phd aff; latest year; latest aff; latest 1st year; latest 1st aff; largest publication gap; numRecords; numLinked; uniqueName; latest year unlinked; noAstroJournal; nonUS' > output/all_years.dat
+rm output/all_years.dat
+
+echo "'name', 'phd year', 'phd bibcode', 'phd aff','latest year','latest aff', 'latest 1st year','latest 1st aff', 'largest publication gap',                  'numRecords','numLinked', 'uniqueName', 'latest year unlinked',                  'noAstroJournal', 'nonUS'" > output/all_years.dat
+
 
 ls output/1*.dat | xargs -I'{}' tail -n +2 '{}' >>  output/all_years.dat
 ls output/2*.dat | xargs -I'{}' tail -n +2 '{}' >>  output/all_years.dat
