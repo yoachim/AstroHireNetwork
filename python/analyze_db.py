4375ed1a319ca0b6857838f8617b3618bc22790e
Peter Yoachim <yoachim@uw.edu>
2016-03-22 15:39:44 -0700

    little refactor

--- python/analyze_db.py
+++ python/analyze_db.py
@@ -45,6 +45,36 @@ class phd_db(object):
         return [fig], ['phdsperyear']
 
 
+    def _hist2curve(self, linked_hist, years, bins, truncate_year=2015):
+        """
+        Take the 2-d array that is number of papers/year and each row is a different astronomer
+        """
+
+        nPhD = linked_hist.shape[0]
+        # Curve that accounts for sometimes it appears an author has
+        # left the field, but they actually will publish again later
+        nz = np.where(linked_hist != 0)
+        raw_active = linked_hist*0
+        raw_active[nz] += 1
+        # Curve where we know the future
+        still_active = np.add.accumulate(linked_hist[:,::-1], axis=1)[:,::-1]
+        still_active[np.where(still_active > 0)] = 1
+
+        # Calculate total number of potential authors in a given year bin.
+        hist_norm = still_active*0.
+        for year in np.unique(years):
+            oneYear = np.where(years == year)[0]
+            hist_norm[oneYear,:] += 1
+            yb = bins+year
+            left_limit = np.where(yb > truncate_year)[0].min()
+            hist_norm[oneYear,left_limit+1:] = 0
+
+        hist_norm = np.sum(hist_norm,axis=0)
+        still_active = np.sum(still_active, axis=0)/hist_norm
+        raw_active = np.sum(raw_active, axis=0)/hist_norm
+
+        return still_active, raw_active
+
     def plot_retention_curve(self, first_author=False):
         """
         return the retention curves as figures.
@@ -54,43 +84,27 @@ class phd_db(object):
         year_bins = np.arange(28)-years_pre_phd-0.5
         bins = year_bins[:-1]+.5
 
-        truncate_year = 2015
-
         fig,ax = plt.subplots()
 
-        year_mins = np.arange(1997,2011+2,2)
-        year_maxes = np.arange(1998,2012+2,2)
+        year_mins = np.arange(1999,2011+2,2)
+        year_maxes = np.arange(2000,2012+2,2)
         colors = [ plt.cm.jet(x) for x in np.linspace(0, 1, year_mins.size) ]
+
+
+        # Can use all_hist as well, fa_hist, fa_linked_hist
 
         for year_min, year_max, color in zip(year_mins, year_maxes, colors):
             label = str(year_min)[-2:]+'-'+str(year_max)[-2:]
             condition = (self.astro_df.phd_year >= year_min) & (self.astro_df.phd_year <= year_max)
+            condition_un = (self.astro_df.phd_year >= year_min) & (self.astro_df.phd_year <= year_max) & (self.astro_df.uniqueName)
             # need to convert "array-of-lists" to 2D array.
             linked_hist = np.array(self.astro_df[condition]['linked_hist'].values.tolist())
+            linked_hist_un = np.array(self.astro_df[condition_un]['linked_hist'].values.tolist())
             years = self.astro_df[condition]['phd_year'].values
-            nPhD = linked_hist.shape[0]
 
-            # Curve that accounts for sometimes it appears an author has
-            # left the field, but they actually will publish again later
-            nz = np.where(linked_hist != 0)
-            raw_active = linked_hist*0
-            raw_active[nz] += 1
-            # Curve where we know the future
-            still_active = np.add.accumulate(linked_hist[:,::-1], axis=1)[:,::-1]
-            still_active[np.where(still_active > 0)] = 1
+            still_active, raw_active = self._hist2curve(linked_hist, years, bins)
 
-            # Calculate total number of potential authors in a given year bin.
-            hist_norm = still_active*0.
-            for year in np.unique(years):
-                oneYear = np.where(years == year)[0]
-                hist_norm[oneYear,:] += 1
-                yb = bins+year
-                left_limit = np.where(yb > truncate_year)[0].min()
-                hist_norm[oneYear,left_limit+1:] = 0
 
-            hist_norm = np.sum(hist_norm,axis=0)
-            still_active = np.sum(still_active, axis=0)/hist_norm
-            raw_active = np.sum(raw_active, axis=0)/hist_norm
             # XXX--need to make error bars go in 2 directions. Use the
             # Unique names as another test.
             yerr = still_active-raw_active
